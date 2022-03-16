c8f3ea9bfd6a8469ed224c8598128f6fbb830af7
naadir77 <naadirinho@gmail.com>
2022-03-16 13:11:46 +0300

    updated the .env file removed the port=5000

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use(express.json());
 app.use(morgan("tiny"));
 // app.use(cors());
 
+const port = process.env.PORT || 5000;
+
 //* task route called.
 // const taskRoute = require("./routes/tasks-route");
 // const bookRoute = require("./routes/books-route");
@@ -20,6 +22,6 @@ app.get("/", (req, res) => {
     res.send("hello world");
 });
 
-app.listen(process.env.PORT || 5000, () =>
+app.listen(port, () =>
     console.log(`server is running on port ${process.env.PORT}`)
 );
