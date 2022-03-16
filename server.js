2c3b196b819f0e369b49edf9764f17bf577b8037
naadir77 <naadirinho@gmail.com>
2022-03-16 12:20:31 +0300

    added the dotenv package

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 require("./config/db");
-
+require("dotenv").config();
 const app = express();
 
 app.use(express.json());
