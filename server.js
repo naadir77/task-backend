179832565087b61fd1ae9dccf26fe6661e179767
naadir77 <naadirinho@gmail.com>
2022-03-16 12:10:54 +0300

    updated the server.js

--- server.js
+++ server.js
@@ -7,14 +7,18 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan("tiny"));
-app.use(cors());
+// app.use(cors());
 
 //* task route called.
-const taskRoute = require("./routes/tasks-route");
-const bookRoute = require("./routes/books-route");
+// const taskRoute = require("./routes/tasks-route");
+// const bookRoute = require("./routes/books-route");
 
-app.use("/api/task/", taskRoute);
-app.use("/api/book/", bookRoute);
+// app.use("/api/task/", taskRoute);
+// app.use("/api/book/", bookRoute);
+
+app.get("/", (req, res) => {
+    res.json("hello world");
+});
 
 app.listen(process.env.PORT, () =>
     console.log("server is running on port 2022..")
